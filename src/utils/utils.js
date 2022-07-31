a7744bb8b684e666b93d0bb2455d533fa2383cd5
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-07-31 22:34:14 +0430

    add details of shapes

---
+++ src/utils/utils.js
@@ -0,0 +1,10 @@
+function loadShapeDetails(details) {
+    var result = {};
+    
+    result['row'] = details['data'].length;
+    result['column'] = details['data'][0].length;
+    result['matrix'] = details['data'];
+    result['rotate'] = details['rotate'];
+
+    return result;
+}
