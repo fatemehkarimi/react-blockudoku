8e01eca2ed6376728c330a5b77f46c9176017f4d
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-08-08 23:11:21 +0430

    set number of shapes on gameboard in configs

--- src/game/game.js
+++ src/game/game.js
@@ -3,6 +3,7 @@ import { useDragDropManager, useDrop } from 'react-dnd';
 import Shape from '../components/shape/shape';
 import GameBoard from './game_board/gameBoard';
 import { loadShapeDetails } from '../utils/utils';
+import appConfig from '../config/config.json';
 import shapeDetails from '../data/shape_details.json';
 import './game.scss';
 
@@ -12,17 +13,41 @@ function getRandomShape() {
   return shapeDetails[idx];
 }
 
+function getRandomShapeList(size) {
+  var result = [];
+
+  for(var i = 0; i < size; ++i)
+    result.push(getRandomShape());
+  return result;
+}
+
+function getShapeListDetails(shapeList) {
+  var result = [];
+  for(var shape of shapeList)
+    result.push(loadShapeDetails(shape));
+  return result;
+}
+
+function createNewShapeListWithDetails(size) {
+  var result = getRandomShapeList(size);
+  result = getShapeListDetails(result);
+  return result;
+}
+
 
 function Game() {
+  const numShapesOnBoard = appConfig["game"]["num-shapes-on-board"];
+
   const gameBoardRef = useRef(null);
   const dragDropManager = useDragDropManager();
   const monitor = dragDropManager.getMonitor();
+
   const [boardBounding, setBoardBounding] = useState({});
-  const [shapes, setShapes] = useState([
-    loadShapeDetails(getRandomShape()),
-    loadShapeDetails(getRandomShape()),
-    loadShapeDetails(getRandomShape())
-  ]);
+  const [shapeBounding, setShapeBounding] = useState({});
+  const [currentDraggingShapeId, setCurrentDraggingShapeId] = useState(null);
+
+  const [shapes, setShapes] = useState(
+      createNewShapeListWithDetails(numShapesOnBoard));
 
   const getPosition = () => {
     setBoardBounding(
@@ -44,16 +69,26 @@ function Game() {
 
   useEffect(() => monitor.subscribeToOffsetChange(() => {
     const offset = monitor.getClientOffset();
-    console.log(offset);
   }), [monitor]);
 
-  const handleDrop = (id) => {}
+  useEffect(() => {
+    if(!currentDraggingShapeId)
+      return;
 
-  const [{isOver, offset}, dropElement] = useDrop(() => ({
+    
+  }, [currentDraggingShapeId]);
+
+  const handleDrop = (id) => {
+    console.log("id = ", id);
+    setCurrentDraggingShapeId(null);
+  }
+
+  const [{isOver, item, initialClientOffset}, dropElement] = useDrop(() => ({
     accept: "shape",
     drop: (item) => handleDrop(item.id),
     collect: (monitor) => ({
-      isOver: !!monitor.isOver()
+      isOver: !!monitor.isOver(),
+      initialClientOffset: monitor.getInitialClientOffset()
     })
   }));
 
@@ -63,7 +98,10 @@ function Game() {
       <div className='shape-holder'>
         {
           [...Array(3).keys()].map((idx) => {
-            return <Shape id={ idx } details={ shapes[idx] } />
+            return <Shape
+                    id={ idx }
+                    onDrag={ setCurrentDraggingShapeId }
+                    details={ shapes[idx] } />
           })
         }
       </div>
