dab71334bfe3d0caa791a6cccbf5df9ee7cd7e42
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-08-08 23:20:06 +0430

    useComponentBoundingRect is a hook that returns the boundingRect of any arbitrary component

---
+++ src/hooks/useComponentBoundingRect.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+export const useComponentBoundingRect = (componentRef) => {
+  const [boundingRect, setBoundingRect] = useState();
+
+  const getPosition = () => {
+    setBoundingRect(
+      componentRef.current.getBoundingClientRect()
+    );
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", getPosition, true);
+
+    return () => {
+      window.removeEventListener("resize", getPosition, true);
+    };
+  }, []);
+  
+
+  useEffect(() => {
+    getPosition();
+  }, []);
+
+  return { boundingRect };
+}
