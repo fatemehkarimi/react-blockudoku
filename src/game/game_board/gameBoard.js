2301112c4e2cac6b9a74d36270aaa06af54a4399
fatemeh karimi <fatemehkarimi.001101@gmail.com>
2022-08-13 15:08:11 +0430

    display correct shadow of object on board

--- src/game/game_board/gameBoard.js
+++ src/game/game_board/gameBoard.js
@@ -5,6 +5,22 @@ import { EMPTY, FILLABLE, FILL } from '../../constants';
 import './gameBoard.scss';
 
 
+function getBoardMatrix(boardMatrix, boardIdx, boardSize) {
+  var result = [];
+  var row = Math.floor(boardIdx / boardSize);
+  var col = boardIdx % boardSize;
+
+  for(var i = 0; i < boardSize; ++i)
+    for(var j = 0; j < boardSize; ++j) {
+      var tmp_i = row * boardSize + i;
+      var tmp_j = col * boardSize + j;
+      result.push(boardMatrix[tmp_i][tmp_j]);
+    }
+  
+  return result;
+}
+
+
 function GameBoard(props, ref) {
   const size = Math.sqrt(props.matrix.length);
 
@@ -17,7 +33,8 @@ function GameBoard(props, ref) {
               <Board key={ i } size={ size }>
                 {
                   [...Array(size ** 2).keys()].map((j) => {
-                    const status = props.matrix[i][j];
+                    const matrix = getBoardMatrix(props.matrix, i, size);
+                    const status = matrix[j];
                     var statusStyle = '';
 
                     if(status == EMPTY)
